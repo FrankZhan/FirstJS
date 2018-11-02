211fa676c971cc46ca9721f0298f4fae2503d4e2
FrankZhan <2283226035@qq.com>
2018-11-02 16:23:41 +0800

    first

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function greeteMe(yourname) {
-   console.log('hello'+yourname);
-}
-
-greeteMe('world')
+let f = function () {
+    let len = arguments.length;
+    console.log(len);
+    let result = true;
+    for (let i in arguments) {
+        result = result && arguments[i];
+    }
+    console.log(result);
+};
+var p1 = true;
+var p2 = false;
+var p3 = true;
+var a = 'f(p1, p2, p3)';
+eval(a)
